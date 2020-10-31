8b6202056f838eac6b866e457b341295e6744c7f
x14gauravg <x14gauravg@iima.ac.in>
2020-11-01 02:59:05 +0530

    Secondary Index

--- src/main/java/com/ppe/db/helper/FakeCustomer.java
+++ src/main/java/com/ppe/db/helper/FakeCustomer.java
@@ -15,11 +15,11 @@ import java.util.List;
 public class FakeCustomer {
 
     // Department
-    @PartitionKey
-    private String PARTITION_KEY;
+    private  String PARTITION_KEY;
 
-    @SortKey
     private String SORT_KEY;
+
+    private List<FakePicture> p;
 
 
     private String ID;
@@ -37,9 +37,29 @@ public class FakeCustomer {
 
     private List<String> names;
     Integer version;
-    private FakePicture fakePicture;
+
+    public FakePicture getImg() {
+        return img;
+    }
+
+
+    public void setImg(FakePicture img) {
+        this.img = img;
+    }
+
+    private FakePicture img;
 
     private Integer junk;
+
+    // Update x = x + 10
+
+    // Get  Object ,  X = 40, Version = 1
+    // Update - X = 50, version = 1 only update
+    //
+
+    // Insert an item - i want it to fail if PK and SK are already there
+
+    //
 
 
     @DynamoDbVersionAttribute
@@ -48,19 +68,22 @@ public class FakeCustomer {
     }
 
 
+    // comma separated list
+    @DynamoDbSecondarySortKey(indexNames = {"TEST_IDX"})
     @DynamoDbPartitionKey
     public String getPARTITION_KEY() {
         return PARTITION_KEY;
     }
 
+    @DynamoDbSecondaryPartitionKey(indexNames = "TEST_IDX")
     @DynamoDbSortKey
     public String getSORT_KEY() {
         return SORT_KEY;
     }
 
-    @DynamoDbAttribute("Pictures")
-    public FakePicture getFakePicture() { return fakePicture;}
-    public void setFakePicture(FakePicture fakePicture) {this.fakePicture = fakePicture;}
+//    @DynamoDbAttribute("Pictures")
+//    public FakePicture getFakePicture() { return fakePicture;}
+//    public void setFakePicture(FakePicture fakePicture) {this.fakePicture = fakePicture;}
 
     @DynamoDbIgnore
     public Integer getJunk() { return junk; }
